c9a32b4bd686b904d512b3ed1f5b786cb0b0a172
youhou4333 <907586967@qq.com>
2022-12-25 15:43:04 +0800

    2nd modify

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git the 2nd time!");
+        System.out.println("Hello Git the 3rd time!");
     }
 }
